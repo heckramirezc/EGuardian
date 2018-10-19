a586f3345ec2d65639b3f806e52cb8cf0765da17
heckramirezc <heckramirezc@aol.com>
2018-10-19 14:18:33 -0600

    First commit

---
+++ EGuardian/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EGuardian.Models;
+
+namespace EGuardian.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
